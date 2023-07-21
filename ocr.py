8b61a24d0a8682c4f34b7290acd24f313467bd8a
“tanishkarao16” <tanishkarao16@gmail.com>
2023-07-21 22:12:36 +0530

    attaching gtesseract in code

--- ocr.py
+++ ocr.py
@@ -1,28 +1,30 @@
-import certifi
-import os
-os.environ['SSL_CERT_FILE'] = certifi.where()
 import streamlit as st
 from PIL import Image
 import pytesseract
+import shutil
 
 st.title("Optical Character Recognition.")
-
 st.markdown("extracting text from image")
 
-image = st.file_uploader("Upload your image over here", type=['png','jpg','jpeg'])
+# Find the path to the Tesseract executable dynamically
+tesseract_cmd = shutil.which('tesseract')
+if tesseract_cmd is None:
+    st.error("Tesseract not found. Please ensure it is installed and accessible.")
+    st.stop()
+
+image = st.file_uploader("Upload your image over here", type=['png', 'jpg', 'jpeg'])
 
 if image is not None:
     input_image = Image.open(image)
     st.image(input_image)
-    
-    with st.spinner("Runningg"):
+
+    with st.spinner("Running"):
+        pytesseract.pytesseract.tesseract_cmd = tesseract_cmd
         
-        config = ('-l eng --oem 1 --psm 3')
+        # Perform OCR using pytesseract
         result = pytesseract.image_to_string(input_image)
-        
-        
+
         st.write(result)
-        
+
     st.success("Done")
     st.balloons()
-    
